e0d8f4beae7b2f584fd36655ebfc966c7a026a1c
Brunhild <nechwi1121@gmail.com>
2018-09-14 14:08:37 +0900

    플레이어 캐릭터의 이동 및 점프 구현

--- Assets/Scripts/CInputMovement.cs
+++ Assets/Scripts/CInputMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class CInputMovement : MonoBehaviour
 {
     public KeyCode jumpKey = KeyCode.Space;
@@ -9,8 +10,15 @@ public class CInputMovement : MonoBehaviour
     public float speed = 6.4f;
     public float keyHoldSecond = 0.5f;
 
+    [Header("Sensitive Jump 사용 여부")]
+    public bool useSensitiveJump = true;
+
+    [Header("일반 점프용")]
     public float jumpForce = 20.0f;
+
+    [Header("Sensitive Jump 시작 Force")]
     public float startJumpForce = 10.0f;
+    [Header("Sensitive Jump 가중 Force")]
     public float additiveJumpForce = 50.0f;
 
     public int maxJumpCount = 1;
@@ -40,13 +48,20 @@ public class CInputMovement : MonoBehaviour
 
     private void Start()
     {
+
     }
 
     private void Update()
     {
         InputMove();
-        //InputJump();
-        InputSensitivityJump();
+        if (useSensitiveJump)
+        {
+            InputSensitivityJump();
+        }
+        else
+        {
+            InputJump();
+        }
 
         isJump = _isJump;
         isGround = _isGround;
@@ -60,15 +75,23 @@ public class CInputMovement : MonoBehaviour
     private void InputMove()
     {
         float h = Input.GetAxisRaw("Horizontal");
-
-        if (Mathf.Abs(h) > 0)
+        float absHorizon = Mathf.Abs(h);
+        if (absHorizon > 0)
+        {
             _spriteRenderer.flipX = (h == 1) ? false : true;
+            _animator.SetBool("Move", true);
+        }
+        else
+        {
+            _animator.SetBool("Move", false);
+        }
 
         _rigidbody2D.velocity = new Vector2(h * speed, _rigidbody2D.velocity.y);
-        _animator.SetFloat("Horizontal", h);
+        _animator.SetFloat("Horizontal", absHorizon);
         _animator.SetFloat("Vertical", _rigidbody2D.velocity.y);
     }
 
+    #region 일반 점프
     private void InputJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -88,7 +111,9 @@ public class CInputMovement : MonoBehaviour
         _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         _animator.SetTrigger("Jump");
     }
+    #endregion
 
+    #region 버튼을 누르는 시간에 따른 민감도 점프
     private void InputSensitivityJump()
     {
         if (Input.GetKeyDown(jumpKey))
@@ -109,6 +134,7 @@ public class CInputMovement : MonoBehaviour
             _keyHoldElipse = 0.0f;
         }
 
+        // 현재 최대 점프수를 넘지 않고 점프키를 누르고 있는 동안
         if (Input.GetKey(jumpKey) && _currentJumpCount <= maxJumpCount)
         {
             if (_keyHoldElipse < keyHoldSecond)
@@ -118,13 +144,14 @@ public class CInputMovement : MonoBehaviour
             }
         }
     }
+    #endregion
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Ground")
         {
             _isGround = true;
-            _animator.SetBool("IsGround", _isGround);
+            _animator.SetBool("Ground", _isGround);
             _currentJumpCount = 0;
             _isJump = false;
         }
@@ -135,7 +162,7 @@ public class CInputMovement : MonoBehaviour
         if (collision.transform.tag == "Ground")
         {
             _isGround = false;
-            _animator.SetBool("IsGround", _isGround);
+            _animator.SetBool("Ground", _isGround);
         }
     }
 }
